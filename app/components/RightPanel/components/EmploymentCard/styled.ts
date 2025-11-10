a96b8aafaf02eb81d007380bc4fd1d44a0934929
Christian Baker <cbaker87@gmail.com>
2025-11-10 14:55:05 +0000

    More styling changes

--- app/components/RightPanel/components/EmploymentCard/styled.ts
+++ app/components/RightPanel/components/EmploymentCard/styled.ts
@@ -1,56 +1,63 @@
+import { colors, fonts } from '@/styles/utils';
 import styled from 'styled-components';
 
 export const Card = styled.div`
-  border-left: 3px solid #667eea;
-  padding-left: 30px;
-  margin-bottom: 40px;
+  border-left: 2px solid ${colors.electricBlue};
+  padding-left: 1.875rem;
+  margin-bottom: 2.5rem;
   position: relative;
 
   &::before {
     content: '';
     position: absolute;
-    left: -7px;
+    left: -0.4375rem;
     top: 0;
-    width: 11px;
-    height: 11px;
-    background: #667eea;
+    width: 0.6875rem;
+    height: 0.6875rem;
+    background: ${colors.electricBlue};
     border-radius: 50%;
   }
 `;
 
 export const Title = styled.h3`
   font-size: 1.6rem;
-  color: #ccd6f6;
-  margin-bottom: 5px;
+  color: ${colors.lightGray};
+  margin-bottom: 0.3125rem;
   font-weight: 600;
+  font-family: ${fonts.heading};
 `;
 
 export const Company = styled.div`
-  color: #667eea;
+  color: ${colors.electricBlue};
   font-size: 1.1rem;
   font-weight: 600;
-  margin-bottom: 5px;
+  margin-bottom: 0.3125rem;
+  font-family: ${fonts.mono};
+  letter-spacing: 0.5px;
 `;
 
 export const Period = styled.div`
-  color: #8892b0;
+  color: ${colors.mediumGray};
   font-size: 0.9rem;
-  margin-bottom: 15px;
+  margin-bottom: 0.9375rem;
+  font-family: ${fonts.body};
 `;
 
 export const Responsibilities = styled.ul`
-  color: #8892b0;
-  margin-left: 20px;
+  color: ${colors.mediumGray};
+  margin-left: 1.25rem;
   list-style-type: disc;
 `;
 
 export const Responsibility = styled.li`
-  margin-bottom: 8px;
+  margin-bottom: 0.5rem;
   line-height: 1.6;
+  font-family: ${fonts.body};
 `;
 
 export const Description = styled.p`
-  color: #8892b0;
-  margin-bottom: 15px;
+  color: ${colors.mediumGray};
+  margin-bottom: 0.9375rem;
   line-height: 1.7;
+  font-family: ${fonts.body};
 `;
