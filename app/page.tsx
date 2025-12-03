a1b248c607eee8f12bcc49035eed69d4f83df107
Christian Baker <cbaker87@gmail.com>
2025-12-03 13:36:00 +0000

    Enable ISR

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import { getPortfolio } from '@/lib/api';
 import { PortfolioClient } from './PortfolioClient';
 
+// Enable ISR: revalidate every 15 minutes (900 seconds)
+export const revalidate = 900;
+
 export default async function Portfolio() {
   const { employments, projects, leftPanelData } = await getPortfolio();
 
--- lib/apollo-client.ts
+++ lib/apollo-client.ts
@@ -2,9 +2,15 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 const httpLink = new HttpLink({
   uri: `${process.env.NEXT_PUBLIC_STRAPI_URL}/graphql`,
+  fetch,
 });
 
 export const apolloClient = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache(),
+  defaultOptions: {
+    query: {
+      fetchPolicy: 'no-cache',
+    },
+  },
 });
